025ad2a2e1d12ffc3ff44a4405b16d3b62a03e63
Navdeep_Mishra <navdeepmishra82734@gmail.com>
2021-10-17 22:35:14 +0530

    added json server support for testing

--- src/App.js
+++ src/App.js
@@ -3,29 +3,58 @@ import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import Add from './components/Add'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 function App() {
+  
   const [tasks,setTask] = useState([
 		
 	])
-  const deltask = (id) =>
+  useEffect(()=>{
+   const getTask = async ()=>{
+     const tasks = await fetchTask();
+     setTask(tasks)
+     console.log('jasljfasdjfasdjfaslkjdfaskdfjasjf')
+   }    
+   getTask()
+  },[])
+  const fetchTask = async () =>{
+    const res = await fetch('http://localhost:5000/tasks')
+    let res_json = await res.json();
+
+    return res_json
+
+  }
+  const deltask = async (id) =>
   {
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'DELETE',
+    })
    
     setTask(tasks.filter((task)=>(task.id!==id)))
   }
+ 
   
   const toggleTask = (id) =>
   {
 	
 	setTask(tasks.map((task)=> task.id===id ? {...task, reminder: !task.reminder} : task))
   }
-  const addTask = (ntask) =>
+  const addTask = async (ntask) =>
   {	  
 	  let id;
 	  id = tasks.length > 0 ? tasks[tasks.length - 1]['id']+1 : 0
 
 	  ntask = {...ntask, id}
-	  setTask([...tasks,ntask])
+    const res = await fetch('http://localhost:5000/tasks',{
+      method: "POST",
+      headers: {
+        'Content-Type':"application/json"
+      },
+      body: JSON.stringify(ntask)
+    })
+    const data = await res.json()
+
+	  setTask([...tasks,data])
   }
   const [showStatus,showAdd] = useState(false)
 	
